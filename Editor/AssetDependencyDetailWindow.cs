854649f11f9e2c18bdf27f1127bcd67bdb0bf9ac
hatch <hatch.tech.eng@gmail.com>
2022-05-31 23:40:19 +0900

    ツール仮作成

---
+++ Editor/AssetDependencyDetailWindow.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Yorozu.EditorTool
+{
+    /// <summary>
+    /// アセットの依存を調べて依存アセットのサイズ等を見る
+    /// </summary>
+    public class AssetDependencyDetailWindow : EditorWindow
+    {
+        [MenuItem("Tools/AssetDependencyDetail")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<AssetDependencyDetailWindow>("AssetDependencyDetail");
+            window.Show();
+        }
+
+        internal static AssetDependencyDetailWindow window;
+
+        private Object _searchTarget;
+        [NonSerialized]
+        private bool _Initialized;
+        [SerializeField]
+        private MultiColumnHeaderState _columnHeaderState;
+        [SerializeField]
+        private TreeViewState _state;
+        private DetailTreeView _treeView;
+
+        private void OnEnable()
+        {
+            window = this;
+        }
+
+        private void OnDisable()
+        {
+            window = null;
+        }
+
+        private void InitIfNeeded()
+        {
+            if (_Initialized)
+                return;
+
+            _state ??= new TreeViewState();
+            
+            var headerState = DetailMultiColumnHeader.Create();
+            if (MultiColumnHeaderState.CanOverwriteSerializedFields(_columnHeaderState, headerState))
+                MultiColumnHeaderState.OverwriteSerializedFields(_columnHeaderState, headerState);
+            _columnHeaderState = headerState;
+            
+            var multiColumnHeader = new DetailMultiColumnHeader(_columnHeaderState);
+            multiColumnHeader.ResizeToFit();
+            
+            _treeView = new DetailTreeView(_state, multiColumnHeader);
+            
+            _Initialized = true;
+        }
+
+        private void OnGUI()
+        {
+            InitIfNeeded();
+            
+            _searchTarget = EditorGUILayout.ObjectField("Target", _searchTarget, typeof(Object), false);
+            using (new EditorGUI.DisabledScope(!_searchTarget))
+            {
+                if (GUILayout.Button("Search Dependency"))
+                {
+                    _treeView.Reload();
+                    Repaint();
+                }
+            }
+            
+            var rect = GUILayoutUtility.GetRect(0, 100000, 0, 100000);
+            _treeView.OnGUI(rect);
+        }
+
+        internal static TreeViewItem SearchDependency()
+        {
+            var root = new TreeViewItem(0, -1, "root");
+            if (window != null && window._searchTarget != null)
+            {
+                var path = AssetDatabase.GetAssetPath(window._searchTarget);
+                var dependencies = AssetDatabase.GetDependencies(path);
+                var depth2Dependencies = dependencies
+                    .SelectMany(AssetDatabase.GetDependencies)
+                    .Distinct();
+                
+                foreach (var assetPath in depth2Dependencies)
+                {
+                    root.AddChild(new DetailTreeViewItem(assetPath));
+                }
+            }
+            
+            return root;
+        }
+    }
+}
---
+++ Editor/DetailTreeView.cs
@@ -0,0 +1,186 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEditor.TreeViewExamples;
+using UnityEngine;
+using UnityEngine.Profiling;
+using Object = UnityEngine.Object;
+
+namespace Yorozu.EditorTool
+{
+    internal class DetailTreeView : TreeView
+    {
+        internal DetailTreeView(TreeViewState state, MultiColumnHeader multiColumnHeader) : base(state, multiColumnHeader)
+        {
+            baseIndent -= 10f;
+            showAlternatingRowBackgrounds = true;
+            showBorder = true;
+            
+            multiColumnHeader.sortingChanged += OnSortingChanged;
+            
+            Reload();
+        }
+
+        /// <summary>
+        /// ソート変更
+        /// </summary>
+        private void OnSortingChanged(MultiColumnHeader multiColumnHeader)
+        {
+            if (GetRows().Count <= 1)
+                return;
+            
+            Sort();
+        }
+
+        private void Sort()
+        {
+            var sortedColumns = multiColumnHeader.state.sortedColumns;
+            if (multiColumnHeader.sortedColumnIndex == -1 ||
+                sortedColumns.Length == 0
+               )
+                return;
+            
+            var cast = rootItem.children.Cast<DetailTreeViewItem>();
+            var ascending = multiColumnHeader.IsSortedAscending(sortedColumns[0]);
+            var items = sortedColumns[0] == 1 ? 
+                cast.Order(i => i.Size, ascending) : 
+                cast.Order(i => i.displayName, ascending);
+                                
+            for (var i = 1; i < sortedColumns.Length; i++)
+            {
+                ascending = multiColumnHeader.IsSortedAscending(sortedColumns[i]);
+                switch (sortedColumns[i])
+                {
+                    case 0:
+                        items = items.ThenBy(i => i.displayName, ascending);
+                        break;
+                    case 1:
+                        items = items.ThenBy(i => i.Size, ascending);
+                        break;
+                }
+            }
+
+            rootItem.children = items.Cast<TreeViewItem>().ToList();
+            Repaint();
+        }
+        
+        protected override TreeViewItem BuildRoot()
+        {
+            var root = AssetDependencyDetailWindow.SearchDependency();
+            SetupDepthsFromParentsAndChildren(root);
+            return root;
+        }
+        
+        protected override IList<TreeViewItem> BuildRows(TreeViewItem root)
+        {
+            if (!root.hasChildren)
+                return new List<TreeViewItem>();
+
+            Sort();
+            return base.BuildRows(root);
+        }
+        
+        protected override void DoubleClickedItem(int id)
+        {
+            EditorGUIUtility.PingObject(EditorUtility.InstanceIDToObject(id));
+        }
+
+        protected override void RowGUI(RowGUIArgs args)
+        {
+            var item = (DetailTreeViewItem) args.item;
+
+            for (var i = 0; i < args.GetNumVisibleColumns(); ++i)
+            {
+                var columnIndex = args.GetColumn(i);
+                switch (columnIndex)
+                {
+                    case 0:
+                        base.RowGUI(args);
+                        break;
+                    // Size
+                    case 1:
+                        EditorGUI.LabelField(args.GetCellRect(columnIndex), item.SizeText);
+                        break;
+                }
+            }
+        }
+    }
+    
+    internal class DetailMultiColumnHeader : MultiColumnHeader
+    {
+        internal static MultiColumnHeaderState Create()
+        {
+            var columns = new MultiColumnHeaderState.Column[]
+            {
+                new MultiColumnHeaderState.Column()
+                {
+                    headerContent = new GUIContent("Name"),
+                    autoResize = true,
+                    allowToggleVisibility = false,
+                },
+                new MultiColumnHeaderState.Column()
+                {
+                    headerContent = new GUIContent("Size"),
+                    autoResize = true,
+                    allowToggleVisibility = false,
+                },
+            };
+         
+            return new MultiColumnHeaderState(columns);
+        }
+        
+        internal DetailMultiColumnHeader(MultiColumnHeaderState state) : base(state)
+        {
+            canSort = true;
+            height = 24f;
+        }
+
+        protected override void AddColumnHeaderContextMenuItems(GenericMenu menu)
+        {
+        }
+    }
+
+    internal class DetailTreeViewItem : TreeViewItem
+    {
+        internal string SizeText;
+        internal long Size;
+        
+        private string _path;
+        
+        internal DetailTreeViewItem(string assetPath)
+        {
+            _path = assetPath;
+            depth = 0;
+            displayName = System.IO.Path.GetFileNameWithoutExtension(assetPath);
+            icon = (Texture2D) AssetDatabase.GetCachedIcon(assetPath);
+            
+            var loadAsset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+            id = loadAsset.GetInstanceID();
+            
+            Size = Profiler.GetRuntimeMemorySizeLong(loadAsset);
+            SizeText = SizeSuffix(Size);
+        }
+        
+        private static readonly string[] SizeSuffixes = {"B", "KB", "MB", "GB"};
+
+        private string SizeSuffix(long value, int decimalPlaces = 1)
+        {
+            if (value < 0)
+            {
+                return "-" + SizeSuffix(-value, decimalPlaces);
+            }
+
+            var i = 0;
+            var dValue = (decimal)value;
+            while (Math.Round(dValue, decimalPlaces) >= 1000 && i < SizeSuffixes.Length - 1)
+            {
+                dValue /= 1024;
+                i++;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}", dValue, SizeSuffixes[i]);
+        }
+    }
+}
